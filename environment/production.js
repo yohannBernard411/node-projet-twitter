31976ed6fc9629130f472cb907714a3d3020ccaa
ocenatcreations <yohannb215@gmail.com>
2021-02-16 11:22:41 +0100

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,5 @@
-const path = requrie('path');
-
 module.exports = {
   dbUrl: 'mongodb+srv://yohann:Guitare512$@cluster0.rhesi.mongodb.net/twitter?retryWrites=true&w=majority',
-  cert: path.join(__dirname, ''),
-  key: path.join(__dirname, '')
+  cert: '/etc/letsencrypt/live/yohann-project.site/fullchain.pem',
+  key: '/etc/letsencrypt/live/yohann-project.site/privkey.pem'
 }
