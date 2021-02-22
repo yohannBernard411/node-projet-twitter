3518c5cf9c3b1a5924ab51dc47d349f380508d53
ocenatcreations <yohannb215@gmail.com>
2021-02-22 10:02:01 +0100

    add express

--- environment/development.js
+++ environment/development.js
@@ -3,5 +3,7 @@ const path = require('path');
 module.exports = {
   dbUrl: 'mongodb+srv://yohann:Guitare512$@cluster0.rhesi.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: path.join(__dirname, '../ssl/local.crt'),
-  key: path.join(__dirname, '../ssl/local.key')
+  key: path.join(__dirname, '../ssl/local.key'),
+  portHttp: 3000,
+  portHttps: 3001
 }
--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,7 @@
 module.exports = {
   dbUrl: 'mongodb+srv://yohann:Guitare512$@cluster0.rhesi.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: '/etc/letsencrypt/live/yohann-project.site/fullchain.pem',
-  key: '/etc/letsencrypt/live/yohann-project.site/privkey.pem'
+  key: '/etc/letsencrypt/live/yohann-project.site/privkey.pem',
+  portHttp: 80,
+  portHttps: 443
 }
