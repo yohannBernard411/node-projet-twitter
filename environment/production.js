39123471e581286784c8d6e30e4772c9889d1866
ocenatcreations <yohannb215@gmail.com>
2021-02-22 11:07:43 +0100

    change cert adress

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,8 @@
 module.exports = {
   dbUrl: 'mongodb+srv://yohann:Guitare512$@cluster0.rhesi.mongodb.net/twitter?retryWrites=true&w=majority',
-  cert: '/etc/letsencrypt/live/www.yohann-project.site/fullchain.pem',
-  key: '/etc/letsencrypt/live/www.yohann-project.site/privkey.pem',
+  
+  cert: '/etc/letsencrypt/renewal/yohann-project.site.conf/fullchain.pem',
+  key: '/etc/letsencrypt/renewal/yohann-project.site.conf/privkey.pem',
   portHttp: 80,
   portHttps: 443
 }
