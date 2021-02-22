fe3eda548280935b93521e4e3d9b1a5821d1af9d
ocenatcreations <yohannb215@gmail.com>
2021-02-22 18:47:04 +0100

    new certificate key

--- environment/production.js
+++ environment/production.js
@@ -1,6 +1,5 @@
 module.exports = {
   dbUrl: 'mongodb+srv://yohann:Guitare512$@cluster0.rhesi.mongodb.net/twitter?retryWrites=true&w=majority',
-  
   cert: '/etc/letsencrypt/live/yohann-project.site/fullchain.pem',
   key: '/etc/letsencrypt/live/yohann-project.site/privkey.pem',
   portHttp: 80,
