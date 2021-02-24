8c9b9facd2dd877cad8d4562758f00de1359fabc
ocenatcreations <yohannb215@gmail.com>
2021-02-24 13:01:29 +0100

    clean tree

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const env = require(`../environment/${ process.env.NODE_ENV }`);
 
+mongoose.set('useCreateIndex', true);
+
 mongoose.connect(env.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then( () => {
           console.log('Connection DB twitter ok!');
